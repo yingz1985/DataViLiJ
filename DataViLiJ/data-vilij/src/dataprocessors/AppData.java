0d7fbc823eb2b7c3563f19b35c56b96c15fe4456
Bagel <Bagel@10.1.211.192>
2018-04-06 15:25:27 -0400

    add project

---
+++ DataViLiJ/data-vilij/src/dataprocessors/AppData.java
@@ -0,0 +1,135 @@
+package dataprocessors;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import ui.AppUI;
+import vilij.components.DataComponent;
+import vilij.templates.ApplicationTemplate;
+
+import java.nio.file.Path;
+import settings.AppPropertyTypes;
+import vilij.components.ErrorDialog;
+
+/**
+ * This is the concrete application-specific implementation of the data component defined by the Vilij framework.
+ *
+ * @author Ritwik Banerjee
+ * @see DataComponent
+ */
+public class AppData implements DataComponent {
+
+    private TSDProcessor        processor;
+    private ApplicationTemplate applicationTemplate;
+
+    public AppData(ApplicationTemplate applicationTemplate) {
+        this.processor = new TSDProcessor();
+        this.applicationTemplate = applicationTemplate;
+    }
+
+    @Override
+    public void loadData(Path dataFilePath) {
+        
+        // TODO: NOT A PART OF HW 1
+        
+        
+    }
+    
+
+
+    public void loadData(String dataString) throws Exception{
+        Boolean caught = false;
+        try
+        {
+          processor.processString(dataString);
+        }
+         catch(NumberFormatException e)
+         {
+                       
+                        ErrorDialog dialog = ErrorDialog.getDialog();
+                          dialog.show(AppPropertyTypes.NUMBER_FORMAT_EXCEPTION.toString(),
+                                  applicationTemplate.manager.getPropertyValue(AppPropertyTypes.NUMBER_FORMAT_EXCEPTION.name())+
+                                  "\n"+applicationTemplate.manager.getPropertyValue(
+                        AppPropertyTypes.ERROR_LINE.name())+processor.getLineNum());
+                        caught = true;
+                         
+        }
+        catch(IllegalArgumentException x)
+        {
+                         
+                          ErrorDialog dialog = ErrorDialog.getDialog();
+                          dialog.show(applicationTemplate.manager.getPropertyValue(
+                            AppPropertyTypes.INVALID_DATA_EXCEPTION.name()),
+                            applicationTemplate.manager.getPropertyValue(
+                            AppPropertyTypes.NAME_ERROR_MSG.name())+"\n"+
+                            applicationTemplate.manager.getPropertyValue(
+                            AppPropertyTypes.ERROR_LINE.name())+processor.getLineNum());
+                            caught = true;
+                          
+                         
+        }
+               
+                  
+        catch(NullPointerException k)
+        {
+                        caught=true;
+                        ErrorDialog dialog = ErrorDialog.getDialog();
+                          dialog.show(AppPropertyTypes.IDENTICAL_NAME_EXCEPTION.toString(),
+                                  applicationTemplate.manager.getPropertyValue(AppPropertyTypes.IDENTICAL_NAME_EXCEPTION.name())+
+                                  processor.getName()+"\n"+applicationTemplate.manager.getPropertyValue(
+                                AppPropertyTypes.ERROR_LINE.name())+processor.getLineNum()+
+                                          "\n");
+        }
+        catch (Exception e)
+        {
+                      
+                        caught=true;
+                      ErrorDialog dialog = ErrorDialog.getDialog();
+                      dialog.show(AppPropertyTypes.INVALID_SPACING_ERRORS.toString()
+                      ,applicationTemplate.manager.getPropertyValue(AppPropertyTypes.INVALID_SPACING_ERRORS.name())
+                      +"\n"+applicationTemplate.manager.getPropertyValue(
+                            AppPropertyTypes.ERROR_LINE.name())+processor.getLineNum());
+                       
+        }
+        if(caught)
+        {
+            processor.clear();
+            throw new Exception();
+        }
+         // ((AppUI) applicationTemplate.getUIComponent()).getChart().getData().clear();
+          
+          
+        
+
+    }
+    public int lineNum(){
+        return processor.getLineNum();
+    }
+
+    
+     
+    @Override
+    public void saveData(Path dataFilePath) {
+        // TODO: NOT A PART OF HW 1
+        
+        try (PrintWriter writer = new PrintWriter(Files.newOutputStream(dataFilePath))) {
+            writer.write(((AppUI) applicationTemplate.getUIComponent()).getTextArea().getText());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    
+    public TSDProcessor getProcessor()
+    {
+        return processor;
+    }
+    @Override
+    public void clear() {
+        processor.clear();
+    }
+
+    public void displayData() {
+        processor.toChartData(((AppUI) applicationTemplate.getUIComponent()).getChart());
+    }
+}
---
+++ DataViLiJ/data-vilij/src/ui/DataVisualizer.java
@@ -0,0 +1,47 @@
+package ui;
+
+import actions.AppActions;
+import dataprocessors.AppData;
+import javafx.stage.Stage;
+import vilij.templates.ApplicationTemplate;
+
+import static vilij.settings.InitializationParams.*;
+
+
+/**
+ * The main class from which the application is run. The various components used here must be concrete implementations
+ * of types defined in {@link vilij.components}.
+ *
+ * @author Ritwik Banerjee
+ */
+public final class DataVisualizer extends ApplicationTemplate {
+
+    @Override
+    public void start(Stage primaryStage) {
+        dialogsAudit(primaryStage);
+        if (propertyAudit())
+            userInterfaceAudit(primaryStage);
+    }
+
+    @Override
+    protected boolean propertyAudit() {
+        boolean failed = manager == null || !(loadProperties(PROPERTIES_XML) && loadProperties(WORKSPACE_PROPERTIES_XML));
+        if (failed)
+            errorDialog.show(LOAD_ERROR_TITLE.getParameterName(), PROPERTIES_LOAD_ERROR_MESSAGE.getParameterName());
+        return !failed;
+    }
+
+    @Override
+    protected void userInterfaceAudit(Stage primaryStage) {
+        setUIComponent(new AppUI(primaryStage, this));
+        setActionComponent(new AppActions(this));
+        setDataComponent(new AppData(this));
+
+        uiComponent.initialize();
+    }
+    
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
